0a22d6836ce4d50ec9ec9470a84bc2879110524d
Kamel R <kamel.r@wayne.edu>
2024-01-24 19:02:52 -0500

    added the details to the employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,41 @@
 #include "Employee.h"
+
+void Employee::setName(string n)
+{
+    name = n;
+}
+
+string Employee::getName()
+{
+    return name;
+}
+
+void Employee::setSalary(float s)
+{
+    salary = s;
+}
+
+float Employee::getSalary()
+{
+    return salary;
+}
+
+void Employee::PrintInfo()
+{
+    cout << "Name:" << getName() << endl;
+    cout << "Salary:" << getSalary() << endl;
+
+}
+
+Employee::Employee()
+{
+    setName("not set yet");
+    setSalary(1000);
+
+}
+
+Employee::Employee(string n, float s)
+{
+    setName(n);
+    setSalary(s);
+}
